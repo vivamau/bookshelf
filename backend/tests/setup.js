6446315bd0cde68b3c9826c0f7b2a0f52d854ae1
vivamau <maurizio.blasilli@gmail.com>
2026-02-01 11:00:18 +0100

    update test setup to seed users for integration tests

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,11 +1,12 @@
 const db = require('../config/db');
 const runMigrations = require('../run_migrations');
-const seedUserRoles = require('../seed_userroles');
+const seedUsers = require('../seed_users');
 
 const setupTestDb = async () => {
     try {
         await runMigrations(db);
         await seedUserRoles(db);
+        await seedUsers(db);
     } catch (error) {
         console.error("Failed to setup test DB:", error);
         throw error;
